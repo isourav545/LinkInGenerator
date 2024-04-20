4a0528376e4f5f6d47950063e830e62ef7a337ce
isourav545 <souravghosh@Souravs-MacBook-Air.local>
2024-04-20 10:30:11 +0530

    adding faq and footer banner

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-10">
+    <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-10 sm:grid-cols-2 md:grid-cols-3">
     <a href="#">
       <img class="rounded-t-lg" src="https://pbs.twimg.com/media/GLg1PKxW8AAPVPq?format=jpg&name=large" alt="" />
     </a>
